0e26f2e2cf322fb0de1023843e9a82c4a21bdd87
Miguel Matias <migbonamim@gmail.com>
2025-08-12 17:25:27 -0300

    spot

--- app/api/spotify-login/playlists/route.ts
+++ app/api/spotify-login/playlists/route.ts
@@ -1,20 +1,19 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import SpotifyWebApi from "spotify-web-api-node";
 
 export async function GET() {
-  const cookieStore = await cookies();
-  const access_token = cookieStore.get("spotify_access_token")?.value;
-
-  if (!access_token) {
-    return NextResponse.json({ error: "Access token required" }, { status: 400 });
-  }
-
-  const spotifyApi = new SpotifyWebApi();
-  spotifyApi.setAccessToken(access_token);
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  });
 
   try {
-    const playlistsData = await spotifyApi.getUserPlaylists();
+    const { body } = await spotifyApi.clientCredentialsGrant();
+    spotifyApi.setAccessToken(body.access_token);
+
+    const userId = "22uv452vf26v4fxk7auekggyi";
+    const playlistsData = await spotifyApi.getUserPlaylists(userId);
+
     return NextResponse.json(playlistsData.body.items);
   } catch (err) {
     console.error("Error fetching playlists:", err);
